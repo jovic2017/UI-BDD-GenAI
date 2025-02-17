762def5ff7cd716fb36854c3b0840725aa100da0
Jo <465230373@qq.com>
2025-02-18 00:33:54 +0800

    Init

--- javascript/index.js
+++ javascript/index.js
@@ -5,6 +5,27 @@ function setScreenshotHeight(){
     screenDiv.css("height",screenHeight * 0.7);
 }
 
+function getPageSource(){
+    var $ = layui.$;
+    $.get("/api/getPageSource?type=M",{},function(result){
+
+    });
+}
+
+function debug(){
+    var layer = layui.layer;
+    $.post(url,{"oldPwd":oldPwd,"newPwd":newPwd,"id":logonId},function(result){
+        if(result.code=='success'){
+            layer.msg("密码修改成功");
+            layer.close(idx);
+        }else{
+            layer.msg(result.message);
+        }
+
+    });
+}
+
+
 layui.use(['element','layer','form'],function(){
     var $ = layui.$;
     var layer = layui.layer;
